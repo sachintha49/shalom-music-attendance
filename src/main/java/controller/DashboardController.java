cd8d216ce11a730b3370da89f1e61d9ccedef083
Sachintha <snishajith@gmail.com>
2025-12-09 23:54:39 +0530

    Add Navigation From Dashboard

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardController {
+
+    public BorderPane root;
+
+    public void mouseClickScheduleNewClassOnAction(MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ScheduleNewClass.fxml"))));
+        stage.setResizable(false);
+        stage.centerOnScreen();
+    }
+
+    public void mouseClickAddNewStudentOnAction(MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ManageStudent.fxml"))));
+        stage.setResizable(false);
+        stage.centerOnScreen();
+    }
 }
